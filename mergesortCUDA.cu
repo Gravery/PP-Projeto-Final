31d1fa9a03508795fdc76006e38b1ee86790e128
Guilherme Calça <guilhermecalca@hotmail.com>
2023-03-29 20:51:43 -0300

    Adição dos Códigos com openMP e CUDA

---
+++ mergesortCUDA.cu
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+__device__ void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int *L = (int*)malloc(n1 * sizeof(int));
+    int *R = (int*)malloc(n2 * sizeof(int));
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
+    free(L);
+    free(R);
+}
+
+__global__ void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort<<<1,1>>>(arr, l, m);
+        mergeSort<<<1,1>>>(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+void printArray(int A[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr_size;
+    fscanf(stdin, "%d", &arr_size);
+    int *arr = (int*)malloc(arr_size * sizeof(int));
+
+    for(int i = 0; i<arr_size; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+
+    int *d_arr;
+    cudaMalloc((void**)&d_arr, arr_size * sizeof(int));
+    cudaMemcpy(d_arr, arr, arr_size * sizeof(int), cudaMemcpyHostToDevice);
+
+    mergeSort<<<1,1>>>(d_arr, 0, arr_size - 1);
+
+    cudaMemcpy(arr, d_arr, arr_size * sizeof(int), cudaMemcpyDeviceToHost);
+
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+
+    free(arr);
+    cudaFree(d_arr);
+
+    return 0;
+}
--- mergesortOMP.c
+++ mergesortOMP.c
@@ -1,32 +1,24 @@
-/*Código utilizado para merge sort
-https://www.geeksforgeeks.org/merge-sort */
-
-/* C program for Merge Sort */
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
+
 void merge(int arr[], int l, int m, int r)
 {
 	int i, j, k;
 	int n1 = m - l + 1;
 	int n2 = r - m;
 
-	/* create temp arrays */
 	int L[n1], R[n2];
 
-	/* Copy data to temp arrays L[] and R[] */
 	for (i = 0; i < n1; i++)
 		L[i] = arr[l + i];
 	for (j = 0; j < n2; j++)
 		R[j] = arr[m + 1 + j];
 
-	/* Merge the temp arrays back into arr[l..r]*/
-	i = 0; // Initial index of first subarray
-	j = 0; // Initial index of second subarray
-	k = l; // Initial index of merged subarray
+	i = 0; 
+	j = 0; 
+	k = l; 
 	while (i < n1 && j < n2) {
 		if (L[i] <= R[j]) {
 			arr[k] = L[i];
@@ -39,16 +31,12 @@ void merge(int arr[], int l, int m, int r)
 		k++;
 	}
 
-	/* Copy the remaining elements of L[], if there
-	are any */
 	while (i < n1) {
 		arr[k] = L[i];
 		i++;
 		k++;
 	}
 
-	/* Copy the remaining elements of R[], if there
-	are any */
 	while (j < n2) {
 		arr[k] = R[j];
 		j++;
@@ -56,25 +44,30 @@ void merge(int arr[], int l, int m, int r)
 	}
 }
 
-/* l is for left index and r is right index of the
-sub-array of arr to be sorted */
+//Paralelização do mergeSort com OpenMP
 void mergeSort(int arr[], int l, int r)
-{
+{	
+	int m;
 	if (l < r) {
-		// Same as (l+r)/2, but avoids overflow for
-		// large l and h
+
 		int m = l + (r - l) / 2;
+		//Seção paralela que se subdividirá em 2 seções para cala lado do merge
+		#pragma omp parallel sections num_threads(2)
+		{
+			#pragma omp section
+			{
+				mergeSort(arr, l, m);
+			}
 
-		// Sort first and second halves
-		mergeSort(arr, l, m);
-		mergeSort(arr, m + 1, r);
-
+			#pragma omp section
+			{
+				mergeSort(arr, m + 1, r);
+			}
+		}
 		merge(arr, l, m, r);
 	}
 }
 
-/* UTILITY FUNCTIONS */
-/* Function to print an array */
 void printArray(int A[], int size)
 {
 	int i;
@@ -83,9 +76,8 @@ void printArray(int A[], int size)
 	printf("\n");
 }
 
-/* Driver code */
 int main()
-{	
+{
 	int arr_size;
 	fscanf(stdin, "%d", &arr_size);
 	int arr[arr_size];
@@ -96,7 +88,7 @@ int main()
 
 	printf("Given array is \n");
 	printArray(arr, arr_size);
-
+	
 	mergeSort(arr, 0, arr_size - 1);
 
 	printf("\nSorted array is \n");
